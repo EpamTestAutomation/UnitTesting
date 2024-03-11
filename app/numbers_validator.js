5c3cd7cc3ffc638c4ca939c69e40d87f6c1ed1e1
jjimenezg19 <47197113+jjimenezg19@users.noreply.github.com>
2024-03-11 12:02:09 -0500

    Second commit
    Completed all test cases

--- app/numbers_validator.js
+++ app/numbers_validator.js
@@ -1,10 +1,10 @@
 // Define a class named NumbersValidator
 export class NumbersValidator {
     /**
-   * Method to check if a number is even
-   * @param {Number} number - The number to be checked
-   * @return {Boolean} - Returns true if the number is even, false otherwise
-   */
+     * Method to check if a number is even
+     * @param {Number} number - The number to be checked
+     * @return {Boolean} - Returns true if the number is even, false otherwise
+     */
     isNumberEven(number) {
     // Determine the type of the passed argument
         const typeOfVariable = typeof number;
@@ -18,11 +18,11 @@ export class NumbersValidator {
     }
 
     /**
-   *  Method to extract even numbers from an array
-   * @param {Array<Number>} arrayOfNumbers - The array containing numbers to be checked
-   * @return {Array<Number>} - An array of even numbers
-   * @throws {Error} - Throws an error if the input is not an array of numbers
-   */
+     *  Method to extract even numbers from an array
+     * @param {Array<Number>} arrayOfNumbers - The array containing numbers to be checked
+     * @return {Array<Number>} - An array of even numbers
+     * @throws {Error} - Throws an error if the input is not an array of numbers
+     */
     getEvenNumbersFromArray(arrayOfNumbers) {
     // Check if the input is an array and if every item is of type number
         if (Array.isArray(arrayOfNumbers) && arrayOfNumbers.every((item) => typeof item === 'number')) {
@@ -34,11 +34,11 @@ export class NumbersValidator {
     }
 
     /**
-  * Method to check if all elements in an array are numbers
-  * @param {Array<Number>} arrayOfNumbers - The array to be checked
-  * @return {Boolean} - Returns true if all elements are numbers, false otherwise
-  * @throws {Error} - Throws an error if the input is not an array
-  */
+     * Method to check if all elements in an array are numbers
+     * @param {Array<Number>} arrayOfNumbers - The array to be checked
+     * @return {Boolean} - Returns true if all elements are numbers, false otherwise
+     * @throws {Error} - Throws an error if the input is not an array
+     */
     isAllNumbers(arrayOfNumbers) {
     // Check if the argument is an array
         if (!Array.isArray(arrayOfNumbers)) {
@@ -49,11 +49,11 @@ export class NumbersValidator {
     }
 
     /**
-  * Method to check if a value is an integer
-  * @param {Number} n - The value to be checked
-  * @return {Boolean} - Returns true if the value is an integer, false otherwise
-  * @throws {Error} - Throws an error if the input is not a number
-  */
+     * Method to check if a value is an integer
+     * @param {Number} n - The value to be checked
+     * @return {Boolean} - true if value is integer
+     * @throws {Error} - Throws an error if the input is not a number
+     */
     isInteger(n) {
     // If the type of n is not number, throw an error
         if (typeof n !== 'number') {
--- test/numbers-validator/isNumberEven.spec.js
+++ test/numbers-validator/isNumberEven.spec.js
@@ -1,27 +1,16 @@
 /**
  * Importing the 'expect' function from the 'chai' library to perform assertions
- *
  */
 import {expect} from 'chai';
-
-/**
- * Importing functions 'describe', 'beforeEach', 'afterEach' and 'it' from 'mocha' which is a test framework.
- *
- */
-
 
 /**
  * Importing the NumbersValidator class from the relative path '../app/numbers-validator'
  * so we can test its methods.
  */
 import {NumbersValidator} from '../../app/numbers_validator.js';
+import {describe} from 'mocha';
 
 /**
- * Describe block: test suite that content your tests
- *  Test use name
- * it block = individual test: each test should be complete independent
- *      test name and arrow function
- *
  * 'describe' is used to group related tests together into a test suite.
  * Here, it describes a suite of tests for the 'isNumberEven' method of NumbersValidator class.
  */
@@ -53,29 +42,99 @@ describe('isNumberEven positive tests', () => {
        * The string argument describes what the test should do.
        */
     it('should return true when provided with an even number', () => {
-    /**
+        /**
          * Using 'expect' to assert that the 'isNumberEven' method returns true when
          * passed the even number 4. The '.to.be.equal(true)' is the actual assertion check.
          */
         expect(validator.isNumberEven(4)).to.be.equal(true);
     });
 
-    it('should return false when provided with an no even number', () => {
-    /**
+    it('should return false when provided with a not-even number', () => {
+        /**
          * Using 'expect' to assert that the 'isNumberEven' method returns true when
          * passed the even number 4. The '.to.be.equal(true)' is the actual assertion check.
          */
         expect(validator.isNumberEven(5)).to.be.equal(false);
     });
 
-    it('should throw an error when provided a string', () => {
+    it('should throw an error when provided a param different than number', () => {
         expect(() => {
             validator.isNumberEven('4');
         }).to.throw('[4] is not of type "Number" it is of type "string"');
     });
-
-    /**
-     * Additional tests would follow for different test cases, such as testing if an odd number
-     * returns false or if passing a non-number throws an error.
-     */
 });
+
+describe('getEvenNumbersFromArray positive tests', () => {
+    let arrayValidator;
+
+    beforeEach(() => {
+        arrayValidator = new NumbersValidator();
+    });
+
+    afterEach(function() {
+        // Sets the validator variable to null to clean up memory after each test
+        arrayValidator = null;
+    });
+
+    it('should return an Array of even numbers when provided an Array of numbers', () => {
+        expect(arrayValidator.getEvenNumbersFromArray([1, 2, 3, 4, 5, 6, 7, 8])).to.contains( 2, 4, 6, 8);
+    });
+
+    it('should throw error if the when provided an Array different than numbers', () => {
+        expect(() => {
+            arrayValidator.getEvenNumbersFromArray(['1', '2', 'johan']);
+        }).throw('[1,2,johan] is not an array of "Numbers"');
+    });
+});
+
+describe('isAllNumbers positive tests', () => {
+    let arrayValidator;
+
+    beforeEach(() => {
+        arrayValidator = new NumbersValidator();
+    });
+
+    afterEach(function() {
+        // Sets the validator variable to null to clean up memory after each test
+        arrayValidator = null;
+    });
+
+    it('should return true when provided with an Array of numbers', () => {
+        expect(arrayValidator.isAllNumbers([1, 2, 3, 4, 5])).to.be.equal(true);
+    });
+
+    it('should return false when provided with an Array different of numbers', () => {
+        expect(arrayValidator.isAllNumbers(['1', '2', 'test', 'automation'])).to.be.equal(false);
+    });
+
+    it('should throw error when provided param different than Array', () => {
+        expect(() => {
+            arrayValidator.isAllNumbers('Epam');
+        }).throw('[Epam] is not an array');
+    });
+});
+
+describe('isInteger positive tests', () => {
+    let validator;
+
+    beforeEach(() => {
+        validator = new NumbersValidator();
+    });
+
+    afterEach(function() {
+        // Sets the validator variable to null to clean up memory after each test
+        validator = null;
+    });
+
+    it('should return true when provided an integer', () => {
+        expect(validator.isInteger(10)).to.be.equal(true);
+    });
+
+    it('should throw error when provided a param different than Integer', () => {
+        expect(() => {
+            validator.isInteger('Software');
+        }).throw('[Software] is not a number');
+    });
+});
+
+
